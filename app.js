c115137974833a32deeb61fad2753e30a49bc4d4
DiegoRamirezHenriquez <diegoramirezhenriquez@gmail.com>
2023-01-16 14:22:27 -0300

    borrar tarea de la lista

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const btnIngresar     = document.getElementById("btnIngresar");
 let   inputTodo       = document.getElementById("input-item");
 const contenedorLista = document.querySelector(".contenedor-lista");
-const btnchk =document.querySelector(".botonCHK");
-
 
 btnIngresar.addEventListener("click",function(){
     if(inputTodo.value!=""){
-
         var numLista = contenedorLista.childElementCount;
         contenedorLista.insertAdjacentHTML("afterbegin", `<div class='ingresado' id="${numLista}">
-            <div class='item'>
-                <input type="checkbox">
-                <h2>${inputTodo.value}</h2>
-                <button><img src='images/bx-x.svg' alt='borrar'></button>
-            </div>
+            
+            <input type="checkbox">
+            <h2>${inputTodo.value}</h2>
+            <img src='images/bx-x.svg' alt='borrar'>
+            
         </div>`);
         
     }
    
 })
 
+
+contenedorLista.addEventListener("click",(event)=>{
+    console.log(event.target.nodeName);
+    if(event.target.nodeName=='IMG'){
+        borrar(event.target.parentNode.id);
+    }
+    
+})
+
+let borrar= (id)=>{
+    let borrarTarea =document.getElementById(id)
+    contenedorLista.removeChild(borrarTarea);
+}
