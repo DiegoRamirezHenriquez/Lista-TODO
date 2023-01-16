5e565132c4caa04c91f38a98c287ee30f4c6dd02
DiegoRamirezHenriquez <diegoramirezhenriquez@gmail.com>
2023-01-16 13:15:53 -0300

    editado

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const btnIngresar     = document.getElementById("btnIngresar");
 let   inputTodo       = document.getElementById("input-item");
 const contenedorLista = document.querySelector(".contenedor-lista");
+const btnchk =document.querySelector(".botonCHK");
 
 
 btnIngresar.addEventListener("click",function(){
     if(inputTodo.value!=""){
-        
+
         var numLista = contenedorLista.childElementCount;
-        console.log(elCount);
-        contenedorLista.insertAdjacentHTML("afterbegin", "<div class='ingresado'><div class='item' id='"+numLista+"'><h2>"+inputTodo.value+"</h2><div class='botones-item'><button><img src='images/bx-circle.svg' alt='noCheck'></button><button><img src='images/bx-x.svg' alt='borrar'></button><button><img src='images/bx-pencil.svg' alt='editar'></button></div></div></div>");
+        contenedorLista.insertAdjacentHTML("afterbegin", `<div class='ingresado' id="${numLista}">
+            <div class='item'>
+                <input type="checkbox">
+                <h2>${inputTodo.value}</h2>
+                <button><img src='images/bx-x.svg' alt='borrar'></button>
+            </div>
+        </div>`);
         
     }
    
 })
 
-
-
-
-
-
-//saber cual es el ultimo elemento agregado para poder cambiar el id
