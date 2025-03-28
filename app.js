13f9f2137d82e23d23a9252e2019d60e7acaa8e8
DiegoRamirezHenriquez <diegoramirezhenriquez@gmail.com>
2025-03-28 14:29:09 -0300

    cambios con los checkbox

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const btnIngresar     = document.getElementById("btnIngresar");
 let   inputTodo       = document.getElementById("input-item");
 const contenedorLista = document.querySelector(".contenedor-lista");
+const isCheck=document.querySelectorAll('.check');
 
 btnIngresar.addEventListener("click",function(){
     if(inputTodo.value!=""){
         var numLista = contenedorLista.childElementCount;
         contenedorLista.insertAdjacentHTML("afterbegin", `<div class='ingresado' id="${numLista}">
-            <input type="checkbox">
-            <h2>${inputTodo.value}</h2>
+            <input id="check${numLista}" class="check" type="checkbox">
+            <h2  id="tarea${numLista}">${inputTodo.value}</h2>
             <img src='images/bx-x.svg' alt='borrar' class="borrarItem">
             
             </div>`);
@@ -29,3 +30,20 @@ let borrar= (id)=>{
     contenedorLista.removeChild(borrarTarea);
 }
 
+document.addEventListener('change', (event) => {
+    if (event.target.classList.contains('check')) {
+        if (event.target.checked) {
+            let idTarea=event.target.parentNode.id;
+            let Tarea= document.getElementById('tarea'+idTarea)
+            let nombreTarea= document.getElementById('tarea'+idTarea).textContent;
+            Tarea.style.color="gray";
+            Tarea.innerHTML=`<s>${nombreTarea}</s>`;
+        } else {
+            let idTarea=event.target.parentNode.id;
+            let Tarea= document.getElementById('tarea'+idTarea)
+            let nombreTarea= document.getElementById('tarea'+idTarea).textContent;
+            Tarea.style.color="white";
+            Tarea.innerHTML=`${nombreTarea}`;
+        }
+    }
+});
